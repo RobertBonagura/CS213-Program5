67f78aeb1d509b2b747bbeec2f8538c47bb1bdba
Robert <robert.bonagura@gmail.com>
2020-04-23 21:52:39 -0400

    First dent

--- app/src/main/java/com/example/cs213_program5/AdviceActivity.java
+++ app/src/main/java/com/example/cs213_program5/AdviceActivity.java
@@ -1,8 +1,14 @@
+
 package com.example.cs213_program5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class AdviceActivity extends AppCompatActivity {
 
@@ -10,7 +16,32 @@ public class AdviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
+        Intent intent = getIntent();
+        Float bmi = Float.parseFloat(intent.getStringExtra("BMI"));
+        displayAdvice(bmi);
     }
 
+    private void displayAdvice(Float bmi){
+        String result;
+        ImageView image = findViewById(R.id.imageView);
+        TextView advice = findViewById(R.id.adviceTextView);
+
+        //TODO: Find appropriate photos for each case and save files in app/src/main/res/drawable/
+        if (bmi < 18.5){
+            result = "Underweight";
+        } else if (bmi < 25){
+            result = "Normal";
+            image.setImageResource(R.drawable.pizza);
+        } else if (bmi < 30){
+            result = "Overweight";
+        } else {
+            result = "Obese";
+        }
+        advice.setText(result);
+    }
+
+    public void closeActivity(View view){
+        finish();
+    }
 
 }
--- app/src/main/java/com/example/cs213_program5/MainActivity.java
+++ app/src/main/java/com/example/cs213_program5/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.cs213_program5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText weightEditText;
     private EditText heightEditText;
     private TextView calculatedBmiTextView;
+    private RadioButton englishRadioButton;
+    private RadioButton metricRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +23,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculateBMI(android.view.View targetedView){
+    /**
+     * On-Click handler for Calculate BMI Button
+     * Calculate's BMI and then updates BMI textview.
+     * @param targetedView
+     */
+    public void setBMI(android.view.View targetedView){
+        String bmi;
+        try {
+            bmi = calculateBMI();
+        } catch (NumberFormatException e) {
+            // TODO: Display bad input Toast Message (user entered non numeric)
+            return;
+        } catch (ArithmeticException e){
+            // TODO: Display bad input Toast Message (user entered 0 or empty value)
+            return;
+        }
+        calculatedBmiTextView.setText(bmi);
+    }
 
+    /**
+     * On-Click handler for Get Advice Button
+     * @param targetedView
+     */
+    public void startAdviceActivity(android.view.View targetedView){
+        String bmi;
+        try {
+            bmi = calculateBMI();
+        } catch (NumberFormatException e) {
+            // TODO: Toast Message
+            return;
+        } catch (ArithmeticException e){
+            // TODO: Toast Message
+            return;
+        }
+        calculatedBmiTextView.setText(bmi);
+        if (bmi == null || bmi.length() == 0){
+            // TODO: Toast Message
+        } else {
+            Intent intent = new Intent(MainActivity.this, AdviceActivity.class);
+            intent.putExtra("BMI", bmi);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Calculate BMI based on current TextView values.
+     * Returns BMI as a string
+     * @return
+     */
+    private String calculateBMI(){
         weightEditText = findViewById(R.id.weightEditText);
         heightEditText = findViewById(R.id.heightEditText);
         calculatedBmiTextView = findViewById(R.id.calculatedBmiTextView);
 
-        /* Still need to write error handling for receiving non numeric characters*/
         int weight = Integer.parseInt(weightEditText.getText().toString());
         int height = Integer.parseInt(heightEditText.getText().toString());
-        int sum = weight + height;
 
-        String calculationStr = Integer.toString(sum);
-        calculatedBmiTextView.setText(calculationStr);
+        // TODO: Read from Radio Buttons
+        // And set isMetric boolean value appropriately
+        englishRadioButton = findViewById(R.id.englishRadioButton);
+        metricRadioButton = findViewById(R.id.metricRadoButton);
+
+        boolean isMetric = false;
+
+        // TODO: Update weightEditText and heightEditText hint values based on Radio Buttons
+        // This should actually be done in a seperate On-click listener method.
+
+        float bmi;
+        if (isMetric) {
+            bmi = weight / (height * height);
+        } else {
+            bmi = weight * 703 / (height * height);
+        }
+
+        String bmiString = Float.toString(bmi);
+        return bmiString;
     }
 
-    public void displayAdvice(android.view.View targetedView){
-
-    }
 }
